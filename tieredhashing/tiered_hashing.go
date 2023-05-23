f821466556897992e4439bc683c9a66212eac8c2
Aarsh Shah <aarshkshah1992@gmail.com>
2023-05-23 10:01:02 +0400

    quick main set

--- tieredhashing/tiered_hashing.go
+++ tieredhashing/tiered_hashing.go
@@ -14,8 +14,8 @@ import (
 
 // TODO Make env vars for tuning
 const (
-	maxPoolSize     = 100
-	maxMainTierSize = 50
+	maxPoolSize     = 50
+	maxMainTierSize = 25
 	PLatency        = 90
 
 	// main tier has the top `maxMainTierSize` nodes
@@ -25,8 +25,8 @@ const (
 	reasonCorrectness = "correctness"
 
 	// use rolling windows for latency and correctness calculations
-	latencyWindowSize     = 1000
-	correctnessWindowSize = 1000
+	latencyWindowSize     = 500
+	correctnessWindowSize = 500
 
 	// ------------------ CORRECTNESS -------------------
 	// minimum correctness pct expected from a node over a rolling window over a certain number of observations
