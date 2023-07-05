7c552c0b2dc9d216e63a1a496ea1623ac33d205b
ameanasad <amean.asad1999@gmail.com>
2023-07-05 14:47:11 -0400

    go fmt fix

--- tieredhashing/tiered_hashing.go
+++ tieredhashing/tiered_hashing.go
@@ -256,7 +256,7 @@ func (t *TieredHashing) GetPerf() map[string]*NodePerf {
 func (t *TieredHashing) GetSentinelCid(ip string) (string, error) {
 	if node, ok := t.nodes[ip]; ok {
 		if len(node.SentinelCid) > 0 {
-		return node.SentinelCid, nil
+			return node.SentinelCid, nil
 		} else {
 			return "", fmt.Errorf("sentinel cid doesn't exist for node: %s ", ip)
 		}
