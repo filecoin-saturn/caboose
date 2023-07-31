91e6e7dd03ffcc538825013821792916635a2f58
Will Scott <will.scott@protocol.ai>
2023-07-31 19:16:18 +0200

    fix test nit

--- node_ring_test.go
+++ node_ring_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestNodeRing(t *testing.T) {
 	nr := caboose.NewNodeRing()
-	nodes := make([]caboose.Node, 0)
+	nodes := make([]*caboose.Node, 0)
 	for i := 0; i < 100; i++ {
-		nodes = append(nodes, caboose.Node{URL: fmt.Sprintf("node%d", i)})
+		nodes = append(nodes, &caboose.Node{URL: fmt.Sprintf("node%d", i)})
 	}
 
 	for _, n := range nodes {
-		err := nr.Add(&n)
+		err := nr.Add(n)
 		if err != nil {
 			t.Fatalf("adding should always work: %v", err)
 		}
